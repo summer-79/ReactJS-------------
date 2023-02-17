0e23ca1a925b109c725169fc8004a0dd810dbaa5
summer-79 <programs2ming@gmail.com>
2023-02-18 03:38:30 +0900

    230217+12 css의 클래스 명이 다른 파일에서 중복되는 건 문제가 안된다. 랜덤으로 리액트에서 명명해주기 때문

--- react-for-begginners/src/App.js
+++ react-for-begginners/src/App.js
@@ -1,10 +1,10 @@
 import Button from "./Button";
-
+import styles from "./App.module.css";
 function App() {
   return (
     <div className="App">
       <h1>Welcome back!!!</h1>
-      <Button text={"Continue"} />
+      <Button className={styles.title} text={"Continue"} />
     </div>
   );
 }
