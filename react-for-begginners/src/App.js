d7ce8c0c73b04689f17430575bf02b00e9c4aeab
summer-79 <programs2ming@gmail.com>
2023-02-18 04:10:33 +0900

    230217+14

--- react-for-begginners/src/App.js
+++ react-for-begginners/src/App.js
@@ -3,12 +3,23 @@ import { useState, useEffect } from "react";
 function App() {
   const [value, setValue] = useState(0);
   const onClick = () => setValue((prev) => prev + 1);
+
+  const [keyword, setKeyword] = useState("");
+  const onChange = (event) => setKeyword(event.target.value);
+
   console.log("all is rendered");
   useEffect(() => {
-    console.log("only one render");
+    console.log("CALL THE API ... only one render");
   }, []);
+  useEffect(() => {
+    if (keyword != "" && keyword.length > 5) {
+      console.log("SEARCH FOR", keyword);
+    }
+  }, [keyword]);
+
   return (
     <div className="App">
+      <input value={keyword} onChange={onChange} placeholder="Search here..." />
       <h1>{value}</h1>
       <button onClick={onClick}>Click me</button>
     </div>
