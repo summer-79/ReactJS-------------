8a552512365554d47ca6cc7345dadba77b4982ec
summer-79 <programs2ming@gmail.com>
2023-03-04 22:09:58 +0900

    230304

--- movieapp/src/routes/Detail.js
+++ movieapp/src/routes/Detail.js
@@ -1,8 +1,8 @@
 import { useParams } from "react-router-dom";
 
 function Detail() {
-  const param = useParams();
-  console.log(param);
+  const { id } = useParams();
+  console.log(id);
   return <h1>Detail</h1>;
 }
 export default Detail;
