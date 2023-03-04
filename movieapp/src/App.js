fb5c0626b74c4f2dcb0ed067eeb817c58f20e9fe
summer-79 <programs2ming@gmail.com>
2023-03-04 21:53:56 +0900

    230304

--- movieapp/src/App.js
+++ movieapp/src/App.js
@@ -1,7 +1,16 @@
 import "./App.css";
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./routes/Home";
+import Detail from "./routes/Detail";
 function App() {
-  return null;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/movie" element={<Detail />}></Route>
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
